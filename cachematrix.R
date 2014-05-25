f522d5d28d1ffbeac5f609822b1d6826eefb9deb
dgoyzueta <dgoyzueta@gmail.com>
2014-05-25 13:26:35 -0700

    Sending code for assignment 2...

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function creates in the global enviroment the matrix passed as argument
+## and its inverse to be used in other functions
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    cachedMatrix <<- x
+    cachedInverse <<- solve(cachedMatrix)
+    print("Matrix and its inverse created in the global environment")
 }
 
 
-## Write a short comment describing this function
+## This function checks whether the matrix passed as argument has the same
+## values in the global variable cachedMatrix in the global environment. If so,
+## the function returns the inverse from the cache, otherwise, it returns a new
+## calculated inverse and caches this result and the matrix by invoking makeCacheMatrix()
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    
+    if(all(x == cachedMatrix)) {
+        print("Getting the inverse from the cache")
+        return(cachedInverse)
+    }
+    
+    print("Calculating the inverse")
+    newinverse = solve(x)
+    
+    print("Caching the new matrix and its inverse")
+    makeCacheMatrix(x)
+    
+    newinverse
 }
